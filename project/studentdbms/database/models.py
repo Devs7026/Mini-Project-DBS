1312899518b26751fe42005002ed97468dfece12
Devs7026 <d2twins2607@gmail.com>
2025-02-09 23:09:45 +0530

    further improvements

--- project/studentdbms/database/models.py
+++ project/studentdbms/database/models.py
@@ -6,7 +6,7 @@ class Student(models.Model):
     id= models.IntegerField(primary_key=True)
     name= models.CharField(max_length=200)
     dob = models.DateField()
-    email= models.EmailField(max_length = 254, qdefault="default@example.com")
+    email= models.EmailField(max_length = 254, default="default@example.com")
     course= models.CharField(max_length=50)
     cgpa= models.DecimalField(max_digits=2, decimal_places=1)
 
